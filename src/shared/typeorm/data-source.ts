86a7f2835c33f34990080610b8b7871da93fcbc1
MayconInforGames2017 <maycon_gus2009@hotmail.com>
2023-12-02 20:03:14 -0300

    Criando a conexao com o banco

---
+++ src/shared/typeorm/data-source.ts
@@ -0,0 +1,14 @@
+import { DataSource } from "typeorm";
+
+export const AppDataSource = new DataSource({
+    type: "postgres",
+    host: "localhost",
+    port: 5432,
+    username: "postgres",
+    password: "maycon",
+    database: "apivendas",
+    synchronize: true,
+    logging: true,
+    subscribers: [],
+    migrations: [],
+})
