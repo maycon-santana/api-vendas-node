983ff554237cca89a4d4b023363c5f57ce00dce5
MayconInforGames2017 <maycon_gus2009@hotmail.com>
2023-12-03 12:31:06 -0300

    Customizando o tratamento de Erros da Aplicação

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -3,7 +3,6 @@ import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import routes from './routes';
 import AppError from '../errors/AppError';
-import '../typeorm/data-source';
 
 const app = express();
 
