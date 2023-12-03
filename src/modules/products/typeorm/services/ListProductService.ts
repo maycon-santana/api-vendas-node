832312506d75db8ce1e325c77038612396664b99
MayconInforGames2017 <maycon_gus2009@hotmail.com>
2023-12-03 17:13:44 -0300

    Servico de criação e listagem de produtos

---
+++ src/modules/products/typeorm/services/ListProductService.ts
@@ -0,0 +1,15 @@
+import { getCustomRepository } from 'typeorm';
+import { ProductRepository } from '../repositories/ProductsRepository';
+import Product from '../entities/Product';
+
+class ListProductService {
+    public async execute(): Promise<Product[]> {
+        const productRepository = getCustomRepository(ProductRepository);
+
+        const products = productRepository.find();
+
+        return products;
+    }
+} 
+
+export default ListProductService;
